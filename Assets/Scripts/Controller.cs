56d92aa3e85fd0e7c46477033aa3acbf1ba0469f
mamorum <mamorum310@outlook.jp>
2019-05-15 13:50:06 +0900

    更新：クラスを分割

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -3,34 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Controller : MonoBehaviour {
+  static Position Pos(int x, int y) { return new Position(x, y); }
   int[,] board = new int[12, 25];
-  class Block {
-    int rotate; int x0, y0, x1, y1, x2, y2;
-    internal Block(int r, int[] xy0, int[] xy1, int[] xy2) {
-      rotate = r;
-      x0 = xy0[0]; y0 = xy0[1];
-      x1 = xy1[2]; y1 = xy1[3];
-      x2 = xy2[4]; y2 = xy2[5];
-    }
-  }
-  static int[] XY(int x, int y) { return new int[] { x, y }; }
   Block[] block = {
-    new Block(1, XY(0, 0), XY(0, 0), XY(0, 0)), // null
-    new Block(2, XY(0, -1), XY(0, 1), XY(0, 2)), // tetris
-    new Block(4, XY(0, -1), XY(0, 1), XY(1, 1)), // L1
-    new Block(4, XY(0, -1), XY(0, 1), XY(-1, 1)), // L2
-    new Block(2, XY(0, -1), XY(1, 0), XY(1, 1)), // Key1
-    new Block(2, XY(0, -1), XY(-1, 0), XY(-1, 1)), // Key2
-    new Block(1, XY(0, 1), XY(1, 0), XY(1, 1)), // Square
-    new Block(4, XY(0, -1), XY(1, 0), XY(-1, 0)), // T
+    new Block(1, Pos(0, 0), Pos(0, 0), Pos(0, 0)), // null
+    new Block(2, Pos(0, -1), Pos(0, 1), Pos(0, 2)), // tetris
+    new Block(4, Pos(0, -1), Pos(0, 1), Pos(1, 1)), // L1
+    new Block(4, Pos(0, -1), Pos(0, 1), Pos(-1, 1)), // L2
+    new Block(2, Pos(0, -1), Pos(1, 0), Pos(1, 1)), // Key1
+    new Block(2, Pos(0, -1), Pos(-1, 0), Pos(-1, 1)), // Key2
+    new Block(1, Pos(0, 1), Pos(1, 0), Pos(1, 1)), // Square
+    new Block(4, Pos(0, -1), Pos(1, 0), Pos(-1, 0)), // T
   };
-  class Status {
-    int x, y, type, rotate;
-  }
   Status current = new Status();
 
-  void Start() {
+  bool PutBlock(Status s, bool action) {
+    if (board[s.x, s.y] != 0) return false;
+    if (action) board[s.x, s.y] = s.type;
+    for (int i=0; i<3; i++) {
+      int dx = block[s.type].p[i].x;
+      int dy = block[s.type].p[i].y;
+      int r = s.rotate % block[s.type].rotate;
+      for (int j=0; j<r; j++) {
+        int nx, ny;
+        nx = dx; ny = dy; dx = ny; dy = -nx;
+      }
+      if (board[s.x + dx, s.y + dy] != 0) {
+        return false;
+      }
+      if (action) {
+        board[s.x + dx, s.y + dy] = s.type;
+      }
+    }
+    if (!action) PutBlock(s, true);
+    return true;
+  }
 
+  void Start() {
+    //-> Init board
+    for (int x=0; x<12; x++) {
+      for (int y=0; y<25; y++) {
+        if (x==0 || x==11 || y==0) {
+          board[x, y] = 1;
+        } else {
+          board[x, y] = 0;
+        }
+      }
+    }
+    //-> Init Current
+    current.x = 5;
+    current.y = 21;
+    current.type = Random.Range(1, 8); // 1 ～ 7
+    current.rotate = Random.Range(0, 5); // 0 ～ 4
+    PutBlock(current, false);
   }
 
   void Update() {
---
+++ Assets/Scripts/Position.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+internal class Position {
+  internal int x, y;
+  internal Position(int x, int y) {
+    this.x = x; this.y = y;
+  }
+}
